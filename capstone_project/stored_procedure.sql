8b0591a32d35a381c86042b825ed57cf2c79d6f3
hiruthikaraja <hiruthikaraja2004@gmail.com>
2025-09-14 14:12:21 +0530

    capstone_project directory

--- capstone_project/stored_procedure.sql
+++
@@ -1,9 +0,0 @@
-CREATE PROCEDURE CalculateDailySales(IN store INT, IN date_selected DATE)
-BEGIN
-    SELECT s.store_id, st.store_name, SUM(s.quantity * p.price) AS total_sales
-    FROM sales s
-    JOIN products p ON s.product_id = p.product_id
-    JOIN stores st ON s.store_id = st.store_id
-    WHERE s.store_id = store AND s.sale_date = date_selected
-    GROUP BY s.store_id, st.store_name;
-END;
